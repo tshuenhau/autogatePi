42ffff42374ce0c658d09b165980b163946fee42
tshuenhau <tshuenhau@gmail.com>
2022-01-02 00:10:50 +0800

    cron works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { Route, Switch } from "react-router-dom";
 import * as api from "./api";
 
 function App() {
-  return <button onClick={api.instant}>Unlock2</button>;
+  return <button onClick={api.instant}>Unlock</button>;
 }
 
 export default App;
