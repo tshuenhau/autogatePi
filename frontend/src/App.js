4593a04b15f485bcbcc33ac9c5d8ae0e26f6b7a4
tshuenhau <tshuenhau@gmail.com>
2022-01-01 23:24:54 +0800

    test cronjob

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { Route, Switch } from "react-router-dom";
 import * as api from "./api";
 
 function App() {
-  return <button onClick={api.instant}>Instant Unlock</button>;
+  return <button onClick={api.instant}>Unlock</button>;
 }
 
 export default App;
