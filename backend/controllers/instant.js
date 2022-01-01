f22a0f4a9e13e72bbea5ddb885e3d8d15ac1d297
tshuenhau <tshuenhau@gmail.com>
2022-01-01 19:21:53 +0800

    changed delay function

--- backend/controllers/instant.js
+++ backend/controllers/instant.js
@@ -1,6 +1,7 @@
 import { schedule } from "../testdata.js";
 import { addSchedule } from "../testdata.js";
 import { Gpio } from "onoff";
+
 //const Gpio = require("onoff").Gpio;
 var relay = new Gpio(18, "out");
 relay.writeSync(1);
@@ -10,14 +11,13 @@ relay.writeSync(1);
 export const instant = (req, res) => {
   res.send("Instant Unlock");
   console.log("Unlock");
-  //console.log("Instant Unlock @ " + req.body.time);
-  //addSchedule(req.body.time); // this is for testing purposes for the schedule unlock later on
-  //console.log(schedule); // this is for testing purposes for the schedule unlock later on
+  console.log("Instant Unlock @ " + req.body.time);
+  addSchedule(req.body.time); // this is for testing purposes for the schedule unlock later on
+  console.log(schedule); // this is for testing purposes for the schedule unlock later on
   //set gpio to high/low
-  try {
+
+  setTimeout(() => {
     relay.writeSync(0);
-    time.sleep(3);
-  } finally {
-    relay.writeSync(1);
-  }
+  }, 3000);
+  relay.writeSync(1);
 };
