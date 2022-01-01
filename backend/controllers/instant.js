47318bb21ec10ee34ac6cbd078f0aae39dbaf626
tshuenhau <tshuenhau@gmail.com>
2022-01-02 03:03:34 +0800

    prevent unlock request while another is ongoing.

--- backend/controllers/instant.js
+++ backend/controllers/instant.js
@@ -5,17 +5,22 @@ import { Gpio } from "onoff";
 //const Gpio = require("onoff").Gpio;
 var relay = new Gpio(14, "high");
 // import arr from "../index";
-
+var done = true;
 export const instant = (req, res) => {
-  res.send("Instant Unlock");
-  console.log("Unlock");
-  console.log("Instant Unlock @ " + req.body.time);
-  addSchedule(req.body.time); // this is for testing purposes for the schedule unlock later on
-  console.log(schedule); // this is for testing purposes for the schedule unlock later on
-  //set gpio to high/low
+  if (done == true) {
+    done = false;
+    res.send("Instant Unlock");
+    console.log("Unlock");
+    console.log("Instant Unlock @ " + req.body.time);
+    addSchedule(req.body.time); // this is for testing purposes for the schedule unlock later on
+    console.log(schedule); // this is for testing purposes for the schedule unlock later on
+    //set gpio to high/low
 
-  setTimeout(() => {
-    relay.writeSync(1);
-  }, 3000);
-  relay.writeSync(0);
+    //while (relay.readSync() != 0) {}
+    setTimeout(() => {
+      relay.writeSync(1);
+    }, 3000);
+    relay.writeSync(0);
+    done = true;
+  }
 };
