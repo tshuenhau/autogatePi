8d9a6c1519ff796f52e59b46d1b7609d00ac106d
tshuenhau <tshuenhau@gmail.com>
2022-01-01 19:17:57 +0800

    removed time reference

--- backend/controllers/instant.js
+++ backend/controllers/instant.js
@@ -3,13 +3,16 @@ import { addSchedule } from "../testdata.js";
 import { Gpio } from "onoff";
 //const Gpio = require("onoff").Gpio;
 var relay = new Gpio(18, "out");
+relay.writeSync(1);
+
 // import arr from "../index";
 
 export const instant = (req, res) => {
   res.send("Instant Unlock");
-  console.log("Instant Unlock @ " + req.body.time);
-  addSchedule(req.body.time); // this is for testing purposes for the schedule unlock later on
-  console.log(schedule); // this is for testing purposes for the schedule unlock later on
+  console.log("Unlock");
+  //console.log("Instant Unlock @ " + req.body.time);
+  //addSchedule(req.body.time); // this is for testing purposes for the schedule unlock later on
+  //console.log(schedule); // this is for testing purposes for the schedule unlock later on
   //set gpio to high/low
   try {
     relay.writeSync(0);
