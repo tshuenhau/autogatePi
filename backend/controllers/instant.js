e5dec17f351d1e24490ad872ff2ea1b53949ee9d
tshuenhau <tshuenhau@gmail.com>
2022-01-02 03:18:16 +0800

    request timeout test

--- backend/controllers/instant.js
+++ backend/controllers/instant.js
@@ -5,23 +5,18 @@ import { Gpio } from "onoff";
 //const Gpio = require("onoff").Gpio;
 var relay = new Gpio(14, "high");
 // import arr from "../index";
-var done = true;
 export const instant = (req, res) => {
-  if (done == true) {
-    done = false;
-    res.send("Instant Unlock");
-    console.log("Unlock");
-    console.log("Instant Unlock @ " + req.body.time);
-    addSchedule(req.body.time); // this is for testing purposes for the schedule unlock later on
-    console.log(schedule); // this is for testing purposes for the schedule unlock later on
-    //set gpio to high/low
+  req.connection.timeout(10);
+  res.send("Instant Unlock");
+  console.log("Unlock");
+  console.log("Instant Unlock @ " + req.body.time);
+  addSchedule(req.body.time); // this is for testing purposes for the schedule unlock later on
+  console.log(schedule); // this is for testing purposes for the schedule unlock later on
+  //set gpio to high/low
 
-    //while (relay.readSync() != 0) {}
-    setTimeout(() => {
-      relay.writeSync(1);
-    }, 3000);
-    relay.writeSync(0);
-    done = true;
-    res.send("Unlocked");
-  }
+  //while (relay.readSync() != 0) {}
+  setTimeout(() => {
+    relay.writeSync(1);
+  }, 3000);
+  relay.writeSync(0);
 };
