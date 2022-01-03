d45d2bec41fc641e45b785144e8c0a3e68304695
tshuenhau <tshuenhau@gmail.com>
2022-01-04 03:12:52 +0800

    minor ui tweak

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main.js";
-import { useState } from "react";
 import classes from "./App.module.css";
 
 function App() {
-  const [buttonClicked, setButtonClicked] = useState(false);
-
   return (
     <div className={classes.app}>
       <Routes>
