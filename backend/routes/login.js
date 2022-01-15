34fa672404f569fa5a0090c4900a06d7a1f8e3c2
tshuenhau <tshuenhau@gmail.com>
2022-01-15 23:37:14 +0800

    test

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -8,7 +8,7 @@ const router = express.Router();
 let sql = "SELECT password FROM login WHERE password = ?";
 router.post("/", (req, res) => {
   db.get(sql, JSON.stringify(req.body.password), (err, result) => {
-    console.log(req.body.password.String());
+    console.log(req.body.password);
     if (err) {
       return console.error(err.message);
     }
