2cdf71780a4785abb38bbf402bf85bbfdb4dbccf
tshuenhau <tshuenhau@gmail.com>
2022-07-21 17:15:25 +0800

    unlockbackup fix

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,9 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/unlock", unlock);
+
+app.use("/unlockbackup", unlockbackup);
+
 app.use("/authentication", authentication);
 
 const PORT = process.env.PORT || 5000;
